a00fa3566802dca0ad52230d59dd01b318ba4bfe
Bryan De La Cruz <bdelacruz@ecomsur.com>
2024-02-24 12:34:23 -0500

    S14-Página 404 Personalizada

--- src/app/(shop)/category/not-found.tsx
+++ src/app/(shop)/category/not-found.tsx
@@ -1,10 +1,7 @@
-import Link from "next/link";
+import { PageNotFound } from "@/components";
 
 export default function() {
   return (
-    <div>
-      <h1>404 Not found</h1>
-      <Link href="/">Regresar</Link>
-    </div>
+    <PageNotFound/>
   );
 }
