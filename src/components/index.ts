3ff2a0c41fafe26d01bde97790cd12faab8f7361
Bryan De La Cruz <bdelacruz@ecomsur.com>
2024-03-02 13:08:51 -0500

    S14-Menú lateral Primera parte

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 export * from "./ui/top-menu/TopMenu";
 export * from "./ui/not-found/PageNotFound";
 export * from "./ui/title/Title";
+export * from "./ui/sidebar/Sidebar"
 
 export * from "./products/products-grid/ProductsGrid";
 export * from "./products/products-grid/ProductsGridItem";
+
