c5cd95839fecf13fec141d25058d4e415203b15c
Bryan De La Cruz <bdelacruz@ecomsur.com>
2024-03-01 19:36:03 -0500

    S14-Cambiar imagen MouseOver

--- src/components/products/products-grid/ProductsGridItem.tsx
+++ src/components/products/products-grid/ProductsGridItem.tsx
@@ -1,21 +1,33 @@
+"use client"
+
 import { Product } from "@/interfaces"
 import Image from "next/image"
 import Link from "next/link"
+import { useState } from "react"
 
 interface Props {
   product: Product
 }
 
 export const ProductsGridItem = ({ product }: Props) => {
+
+  const [displayImage, setDisplayImage] = useState(product.images[0])
+
   return (
     <div className="rounded-md overflow-hidden fade-in ">
-      <Image
-        src={`/products/${product.images[0]}`}
-        alt={product.title}
-        className="w-full object-cover"
-        width={500}
-        height={500}
-      />
+      <Link
+        className="hover:text-blue-600"
+        href={`/product/${product.slug}`}>
+        <Image
+          src={`/products/${displayImage}`}
+          alt={product.title}
+          className="w-full object-cover rounded"
+          width={500}
+          height={500}
+          onMouseEnter={() => setDisplayImage(product.images[1])}
+          onMouseLeave={() => setDisplayImage(product.images[0])}
+        />
+      </Link>
       <div className="p4 flex flex-col">
         <Link
           className="hover:text-blue-600"
