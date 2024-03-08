c7d18b8ae4370d8b8d9c1235d8dc5d3e0afe66b5
Bryan De La Cruz <bdelacruz@ecomsur.com>
2024-03-07 22:52:18 -0500

    S14-Zustand UI Store

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -2,82 +2,96 @@
 import Link from "next/link"
 import { IoCloseOutline, IoLogInOutline, IoLogOutOutline, IoPeopleOutline, IoPersonOutline, IoSearchOutline, IoShirtOutline, IoTicketOutline } from "react-icons/io5"
 
+import { useUIStore } from "@/store"
+import clsx from "clsx"
+
 export const Sidebar = () => {
+
+  const isSideMenuOpen = useUIStore(state => state.isSideMenuOpen)
+  const closeMenu = useUIStore(state => state.closeSideMenu)
+
   return (
     <div>
-      <div className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30">
-      </div>
-      <div className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-sm">
-      </div>
-      <nav className="fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300">
+      {
+        isSideMenuOpen &&
+        <div className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30" />
+      }
+      {
+        isSideMenuOpen &&
+        <div 
+          className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-sm"
+          onClick={()=>closeMenu()} />
+      }
+      <nav className={clsx("fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300",
+        { "translate-x-full": !isSideMenuOpen })}>
         <IoCloseOutline
           size={50}
           className="absolute top-5 right-5 cursor-pointer"
-          onClick={()=>console.log('click')}
+          onClick={() => closeMenu()}
         />
 
         <div className="relative mt-14">
-          <IoSearchOutline size={20} className="absolute top-2 left-2"/>
+          <IoSearchOutline size={20} className="absolute top-2 left-2" />
           <input type="text" placeholder="Buscar" className="w-full bg-gray-50 rounded pl-10 pr-10 border-b-2 text-xl border-gray-200 focus:outline-none focus:border-blue-500" />
         </div>
 
         <Link
           href="/"
           className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
-          >
-            <IoPersonOutline size={30}/>
-            <span className="ml-3 text-xl">Perfil</span>
-          </Link>
+        >
+          <IoPersonOutline size={30} />
+          <span className="ml-3 text-xl">Perfil</span>
+        </Link>
 
-          <Link
+        <Link
           href="/"
           className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
-          >
-            <IoTicketOutline size={30}/>
-            <span className="ml-3 text-xl">Órdenes</span>
-          </Link>
+        >
+          <IoTicketOutline size={30} />
+          <span className="ml-3 text-xl">Órdenes</span>
+        </Link>
 
-          <Link
+        <Link
           href="/"
           className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
-          >
-            <IoLogInOutline size={30}/>
-            <span className="ml-3 text-xl">Ingresar</span>
-          </Link>
+        >
+          <IoLogInOutline size={30} />
+          <span className="ml-3 text-xl">Ingresar</span>
+        </Link>
 
-          <Link
+        <Link
           href="/"
           className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
-          >
-            <IoLogOutOutline size={30}/>
-            <span className="ml-3 text-xl">Salir</span>
-          </Link>
+        >
+          <IoLogOutOutline size={30} />
+          <span className="ml-3 text-xl">Salir</span>
+        </Link>
 
-          <div className="w-full h-px bg-gray-200 my-10"/>
+        <div className="w-full h-px bg-gray-200 my-10" />
 
-          <Link
+        <Link
           href="/"
           className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
-          >
-            <IoShirtOutline size={30}/>
-            <span className="ml-3 text-xl">Productos</span>
-          </Link>
+        >
+          <IoShirtOutline size={30} />
+          <span className="ml-3 text-xl">Productos</span>
+        </Link>
 
-          <Link
+        <Link
           href="/"
           className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
-          >
-            <IoTicketOutline size={30}/>
-            <span className="ml-3 text-xl">Órdenes</span>
-          </Link>
+        >
+          <IoTicketOutline size={30} />
+          <span className="ml-3 text-xl">Órdenes</span>
+        </Link>
 
-          <Link
+        <Link
           href="/"
           className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
-          >
-            <IoPeopleOutline size={30}/>
-            <span className="ml-3 text-xl">Usuarios</span>
-          </Link>
+        >
+          <IoPeopleOutline size={30} />
+          <span className="ml-3 text-xl">Usuarios</span>
+        </Link>
 
 
 
