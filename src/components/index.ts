4c486ea6af9c5b99308f7bcbae384c0516b46483
Bryan De La Cruz <bdelacruz@ecomsur.com>
2024-03-09 16:27:04 -0500

    S15-Footer Pie de página

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export * from "./ui/top-menu/TopMenu";
 export * from "./ui/not-found/PageNotFound";
 export * from "./ui/title/Title";
 export * from "./ui/sidebar/Sidebar"
+export * from "./ui/footer/Footer"
 
 export * from "./product/size-selector/SizeSelector"
 export * from "./product/quantity-selector/QuantitySelector"
