6fd80d12edf07f9b49bfd3c906f77a99b68091a7
Bryan De La Cruz <bdelacruz@ecomsur.com>
2024-03-09 15:43:38 -0500

    S15-Pantalla de verificación de compra

--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -1,8 +1,89 @@
+import { QuantitySelector, Title } from "@/components";
+import Link from "next/link";
+import { initialData } from '@/seed/seed';
+import Image from "next/image";
+
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+]
 
 export default function () {
   return (
-    <div>
-      <h1>Checkout Page</h1>
+    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+      <div className="flex flex-col w-[1000px]">
+        <Title title="Verificar orden" />
+        <div className="grid grid-cols-1 sm:grid-cols-2 sm:items-start gap-10">
+          {/* Carrito */}
+          <div className="flex flex-col mt-5">
+            <span className="text-xl">Ajustar elementos</span>
+            <Link href="/cart" className="underline mb-5">
+              Editar carrito
+            </Link>
+            {/* Items */}
+            {productsInCart.map(product => (
+              <div key={product.slug} className="flex mb-5">
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  width={100}
+                  height={100}
+                  style={{
+                    width: "100px",
+                    height: "100px"
+                  }}
+                  alt={product.title}
+                  className="mr-5 rounded"
+                />
+                <div>
+                  <p>{product.title}</p>
+                  <p>S/{product.price}</p>
+                  <p className="font-bold">Subtotal: S/{product.price * 3}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          {/* Checkout */}
+          <div className="bg-white rounded-xl shadow-xl p-7">
+            <h2 className="text-2xl mb-2">Dirección de entrega</h2>
+            <div className="mb-10">
+              <p>Bryan De La Cruz</p>
+              <p>Jirón Cavallini 118 dpt 301</p>
+              <p>Distrito: San Borja</p>
+              <p>Provincia: Lima Metropolitana</p>
+            </div>
+            <div className="w-full h-0.5 rounded bg-gray-200 mb-10" />
+            <h2 className="text-2xl mb-2">Resumen de orden</h2>
+            <div className="grid grid-cols-2">
+              <span>N° Productos</span>
+              <span className="text-right">3 artículos</span>
+
+              <span>Subtotal</span>
+              <span className="text-right">S/ 100.00</span>
+
+              <span>Impuestos (18%)</span>
+              <span className="text-right">S/ 18.00</span>
+
+              <span className="mt-5 text-2xl">Total</span>
+              <span className="mt-5 text-2xl text-right">S/ 118.00</span>
+            </div>
+
+            <div className="mt-5 mb-2 w-full">
+              <p className="mb-5">
+                <span className="text-xs">
+                  Al hacer click en "Colocar orden", aceptas nuestros <a href="#" className="underline">términos y condicions</a> y <a href="#" className="underline">política de privacidad</a>
+                </span>
+              </p>
+              <Link
+                className="flex btn-primary justify-center"
+                href="/orders/123"
+              >
+                Colocar orden
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
