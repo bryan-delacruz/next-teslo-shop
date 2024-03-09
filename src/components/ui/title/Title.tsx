26fa9657411019d831b6e5c32f21e4f91328e35e
Bryan De La Cruz <bdelacruz@ecomsur.com>
2024-03-09 15:19:13 -0500

    S15-Pantalla de dirección del cliente

--- src/components/ui/title/Title.tsx
+++ src/components/ui/title/Title.tsx
@@ -2,17 +2,17 @@ import { titleFont } from "@/config/fonts"
 
 interface Props {
   title: string
-  subTitle?: string
+  subtitle?: string
   className?: string
 }
-export const Title = ({ title, subTitle, className }: Props) => {
+export const Title = ({ title, subtitle, className }: Props) => {
   return (
     <div className={`mt-3 ${className}`}>
       <h1 className={`${titleFont.className} antialiased text-4xl font-semibold my-10`}>
         {title}
       </h1>
-      {subTitle &&
-        <h3 className="text-xl mb-5">{subTitle}</h3>}
+      {subtitle &&
+        <h3 className="text-xl mb-5">{subtitle}</h3>}
     </div >
   )
 }
