c3fcc3852349cf2632a8a313076879ee9b7b20cc
Bryan De La Cruz <bdelacruz@ecomsur.com>
2024-03-09 11:46:28 -0500

    S15-Slideshow con Thumbs Parte 1

--- src/components/product/slidesshow/ProductSlidesshow.tsx
+++ src/components/product/slidesshow/ProductSlidesshow.tsx
@@ -1,8 +1,17 @@
 "use client"
+import { useState } from 'react';
 
+import { Swiper as SwiperObj } from "swiper"
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+
+import './slideshow.css';
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
+import Image from 'next/image';
 
 interface Props {
   images: string[]
@@ -11,19 +20,33 @@ interface Props {
 }
 
 export const ProductSlideshow = ({ images, title, className }: Props) => {
+
+  const [thumbsSwiper, setThumbsSwiper] = useState<SwiperObj>();
+
   return (
     <div className={className}>
       <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
+        style={{
+          '--swiper-navigation-color': '#fff',
+          '--swiper-pagination-color': '#fff',
+        } as React.CSSProperties}
+        spaceBetween={10}
+        navigation={true}
+        thumbs={{ swiper: thumbsSwiper }}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper2"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        ...
+        {images.map(image => (
+          <SwiperSlide key={image}>
+            <Image
+              width={1024}
+              height={800}
+              src={`/products/${image}`}
+              alt={title}
+              className='rounded-lg object-fill'
+            />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
