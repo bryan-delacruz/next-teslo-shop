b039b9fe236786cfc7d2ed99f09b8f0ed79f5b1b
Bryan De La Cruz <bdelacruz@ecomsur.com>
2024-03-10 23:40:50 -0500

    S16-Semilla de base de datos Configuración inicial

---
+++ src/seed/seed-database.ts
@@ -0,0 +1,18 @@
+import { initialData } from "./seed"
+
+async function main() {
+
+  console.log(initialData)
+
+
+  console.log("Seed ejecutado correctamente")
+}
+
+(() => {
+
+  if (process.env.NODE_ENV ==="production") return
+
+  main()
+}
+
+)()
