b546a57ce5ad2be68e1ac0583af8cc90421d857a
Bryan De La Cruz <bdelacruz@ecomsur.com>
2024-03-14 00:28:54 -0500

    S16-Prisma Client Borrar contenido de tablas

--- README.md
+++ README.md
@@ -8,7 +8,8 @@
 3. Instalar las dependeicias ```npm install```
 4. Levantar la base de datos ```docker compose up -d```
 5. Correr las migraciones de Prisma ```npx prisma migrate dev```
-6. Correr el proyecto ```npm run dev```
+6. Ejecutar seed ```npm run seed```
+7. Correr el proyecto ```npm run dev```
 
 
 ## Correr en Prod
--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,8 +1,16 @@
 import { initialData } from "./seed"
+import prisma from "../lib/prisma"
 
 async function main() {
 
-  console.log(initialData)
+  // 1. Borrar registros previos
+  await Promise.all(
+    [
+      prisma.productImage.deleteMany(),
+      prisma.product.deleteMany(),
+      prisma.category.deleteMany()
+    ]
+  )
 
 
   console.log("Seed ejecutado correctamente")
@@ -10,7 +18,7 @@ async function main() {
 
 (() => {
 
-  if (process.env.NODE_ENV ==="production") return
+  if (process.env.NODE_ENV === "production") return
 
   main()
 }
