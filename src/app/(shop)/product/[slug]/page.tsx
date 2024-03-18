2e1f07d3b65bb7e9308813f36c4b1cfd0c9627f9
Bryan De La Cruz <bdelacruz@ecomsur.com>
2024-03-18 00:39:06 -0500

    S18-Optimizando Metadata

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -6,10 +6,35 @@ import { getProductBySlug } from "@/actions";
 import { titleFont } from "@/config/fonts";
 
 import { notFound } from "next/navigation";
+import { Metadata, ResolvingMetadata } from "next";
 
 interface Props {
   params: {
     slug: string
+  }
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const slug = params.slug
+
+  // fetch data
+  const product = await getProductBySlug(slug)
+
+  // optionally access and extend (rather than replace) parent metadata
+  // const previousImages = (await parent).openGraph?.images || []
+
+  return {
+    title: product?.title ?? "Producto no encontrado",
+    description: product?.description ?? '',
+    openGraph: {
+      title: product?.title ?? "Producto no encontrado",
+      description: product?.description ?? '',
+      images: [`/products/${product?.images[1]}`],
+    },
   }
 }
 
