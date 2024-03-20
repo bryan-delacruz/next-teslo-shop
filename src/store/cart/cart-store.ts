5d91c61e671bfca84de2fe3eeadaf8a68c995829
Bryan De La Cruz <bdelacruz@ecomsur.com>
2024-03-19 21:48:31 -0500

    S19-Hacer persistente el carrito de compras

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -9,34 +9,40 @@ interface State {
 }
 
 export const useCartStore = create<State>()(
-  (set, get) => ({
+  persist(
+    (set, get) => ({
 
-    cart: [],
+      cart: [],
 
-    addProductToCart: (product: CartProduct) => {
-      const { cart } = get()
+      addProductToCart: (product: CartProduct) => {
+        const { cart } = get()
 
-      console.log(cart)
+        console.log(cart)
 
-      const productInCart = cart.some((item) => item.id === product.id && item.size === product.size)
+        const productInCart = cart.some((item) => item.id === product.id && item.size === product.size)
 
-      if (!productInCart) {
-        set({ cart: [...cart, product] })
-        return
-      }
-
-      const updatedCartProducts = cart.map((item) => {
-        if (item.id === product.id && item.size === product.size) {
-          return {
-            ...item,
-            quantity: item.quantity + product.quantity
-          }
+        if (!productInCart) {
+          set({ cart: [...cart, product] })
+          return
         }
-        return item
-      })
 
-      set({ cart: updatedCartProducts })
+        const updatedCartProducts = cart.map((item) => {
+          if (item.id === product.id && item.size === product.size) {
+            return {
+              ...item,
+              quantity: item.quantity + product.quantity
+            }
+          }
+          return item
+        })
 
+        set({ cart: updatedCartProducts })
+
+      }
+    })
+    ,
+    {
+      name: 'shopping-store',
     }
-  })
+  )
 )
