961a40adb244317ec041a0c1af5998509b279c5f
Bryan De La Cruz <bdelacruz@ecomsur.com>
2024-03-20 20:59:55 -0500

    S19-Carrito de compras vacío

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -37,10 +37,10 @@ export const TopMenu = () => {
         <Link href="/search" className="mx-2">
           <IoSearchOutline className="w-5 h-5" />
         </Link>
-        <Link href="/cart" className="mx-2">
+        <Link href={totalItemsInCart === 0 && loaded ? "/empty" : "/cart"} className="mx-2">
           <div className="relative">
             {
-              loaded && totalItemsInCart > 0 && (<span className="absolute text-xs rounded-full px-1 font-bold -top-2 -right-2 bg-blue-700 text-white">{totalItemsInCart}</span>
+              loaded && totalItemsInCart > 0 && (<span className="fade-in absolute text-xs rounded-full px-1 font-bold -top-2 -right-2 bg-blue-700 text-white">{totalItemsInCart}</span>
               )
             }
             <IoCartOutline className="w-5 h-5" />
