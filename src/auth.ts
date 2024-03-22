0a4d578c23b8375a8b79d9f1250b254b248b2ac2
Bryan De La Cruz <bdelacruz@ecomsur.com>
2024-03-21 23:27:37 -0500

    S20-Configuración de Credentials Provider

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,29 @@
-import type { NextAuthConfig } from 'next-auth';
+import NextAuth, { type NextAuthConfig } from 'next-auth';
+import credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
 
 export const authConfig: NextAuthConfig = {
   pages: {
     signIn: '/auth/login',
     newUser: '/auth/new-user'
   },
-  providers: []
+  providers: [
+    credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        if (!parsedCredentials.success) return null
+
+        const { email, password } = parsedCredentials.data;
+
+        console.log({ email, password });
+
+        return null
+      },
+    }),
+  ]
 }
+
+export const { signIn, signOut, auth } = NextAuth(authConfig)
