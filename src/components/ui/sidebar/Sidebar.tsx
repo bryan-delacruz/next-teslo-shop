f8187d1ac3b78c04e327710e1d180c261a4a7f6f
Bryan De La Cruz <bryan.delacruza@gmail.com>
2024-03-29 16:34:58 -0500

    S20-Mostrar y ocultar opciones del menu

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -14,6 +14,8 @@ export const Sidebar = () => {
 
   const { data: session } = useSession()
 
+  const isAuthenticated = !!session?.user
+
   return (
     <div>
       {
@@ -56,21 +58,26 @@ export const Sidebar = () => {
           <span className="ml-3 text-xl">Órdenes</span>
         </Link>
 
-        <Link
-          href="/auth/login"
-          className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
-        >
-          <IoLogInOutline size={30} />
-          <span className="ml-3 text-xl">Ingresar</span>
-        </Link>
+        {!isAuthenticated &&
+          <Link
+            href="/auth/login"
+            className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+          >
+            <IoLogInOutline size={30} />
+            <span className="ml-3 text-xl">Ingresar</span>
+          </Link>
+        }
 
-        <button
-          className="flex w-full items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
-          onClick={() => logout()}
-        >
-          <IoLogOutOutline size={30} />
-          <span className="ml-3 text-xl">Salir</span>
-        </button>
+        {isAuthenticated &&
+          <button
+            className="flex w-full items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+            onClick={() => logout()}
+          >
+            <IoLogOutOutline size={30} />
+            <span className="ml-3 text-xl">Salir</span>
+          </button>}
+
+
 
         <div className="w-full h-px bg-gray-200 my-10" />
 
