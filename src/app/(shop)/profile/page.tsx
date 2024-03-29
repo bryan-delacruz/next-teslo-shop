1cd5ad33b8e0e57f30435904017113e9b8f63885
Bryan De La Cruz <bryan.delacruza@gmail.com>
2024-03-29 16:50:13 -0500

    S20-Expandir el objeto de sessión

--- src/app/(shop)/profile/page.tsx
+++ src/app/(shop)/profile/page.tsx
@@ -18,6 +18,7 @@ export default async function ProfilePage() {
           JSON.stringify(session.user, null, 2)
         }
       </pre>
+      <h3 className="text-3xl mb-10">{session.user.role}</h3>
     </div>
   );
 }
--- src/auth.config.ts
+++ src/auth.config.ts
@@ -9,6 +9,20 @@ export const authConfig: NextAuthConfig = {
     signIn: '/auth/login',
     newUser: '/auth/new-user'
   },
+
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.data = user
+      }
+      return token
+    },
+    session({ session, token, user }) {
+      console.log({ session, token, user });
+      session.user = token.data as any
+      return session
+    }
+  },
   providers: [
     credentials({
       async authorize(credentials) {
