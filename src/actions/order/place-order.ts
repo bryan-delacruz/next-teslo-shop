fd76fc3c951ee50c9e6dda6ba18b2b3ca02fb375
Bryan De La Cruz <bryan.delacruza@gmail.com>
2024-03-31 12:54:52 -0500

    S22-Transacción: Insertar la dirección

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -73,12 +73,23 @@ export const placeOrder = async (productIds: ProductToOrder[], address: Address)
         }
       }
     })
+    // Validar si el price es cero, entonces lanzar un error
     // 3. Crear la dirección de la orden
+    const { country, ...restAddress } = address
+
+    const orderAddress = await tx.orderAddress.create({
+      data: {
+        ...restAddress,
+        countryId: country,
+        orderId: order.id
+      }
+    })
+
 
     return {
-      order: order,
       updatedProducts: [],
-      orderAddress: {}
+      order: order,
+      orderAddress: orderAddress
     }
   })
 }
