a8a044dd9649b4f374084470664f5ffd22141cdb
Bryan De La Cruz <bryan.delacruza@gmail.com>
2024-03-31 11:43:47 -0500

    S22-Colocar orden: Productos y cantidades

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -2,6 +2,7 @@
 
 import { auth } from "@/auth.config"
 import { Address, Size } from "@/interfaces"
+import prisma from "@/lib/prisma"
 
 interface ProductToOrder {
   productId: string
@@ -11,12 +12,29 @@ interface ProductToOrder {
 
 export const placeOrder = async (productIds: ProductToOrder[], address: Address) => {
   const session = await auth()
-  const userId=session?.user.id
-  if (!userId){
+  const userId = session?.user.id
+
+  // Verificar sesión de usuario
+  if (!userId) {
     return {
-      ok:false,
+      ok: false,
       message: "No hay sesión de usuario"
     }
   }
-  console.log({ productIds, address, userId })
+
+  // Obtener la información de los productos
+  // Nota: Recordar que podemos llevar +2 productos con el mismo ID
+  const products = await prisma.product.findMany({
+    where: {
+      id: {
+        in: productIds.map(p => p.productId)
+      }
+    }
+  })
+  // Calcular los montos
+  const itemsInOrder = productIds.reduce((count, p) => count + p.quantity, 0)
+
+  console.log({ itemsInOrder })
+
+  // Los totales de tax, subtotal y total
 }
