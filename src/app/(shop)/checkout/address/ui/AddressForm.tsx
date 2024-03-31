b5b611ff66ad62a687aba3ea0cacbeb74e88aed3
Bryan De La Cruz <bryan.delacruza@gmail.com>
2024-03-31 00:24:41 -0500

    S21-Cargar la dirección en el servidor

--- src/app/(shop)/checkout/address/ui/AddressForm.tsx
+++ src/app/(shop)/checkout/address/ui/AddressForm.tsx
@@ -1,9 +1,8 @@
 "use client"
 
 import { deleteUserAddress, setUserAddress } from "@/actions"
-import type { Country } from "@/interfaces"
+import type { Address, Country } from "@/interfaces"
 import { useAddressStore } from "@/store"
-import { sleep } from "@/utils"
 import clsx from "clsx"
 import { useSession } from "next-auth/react"
 import { useEffect } from "react"
@@ -23,13 +22,16 @@ type FormInputs = {
 
 interface Props {
   countries: Country[]
+  userStoredAddress?: Partial<Address>
 }
 
-export const AddressForm = ({ countries }: Props) => {
+export const AddressForm = ({ countries, userStoredAddress = {} }: Props) => {
 
   const { handleSubmit, register, formState: { isValid }, reset } = useForm<FormInputs>({
     defaultValues: {
       // Todo: leer de la BD
+      ...(userStoredAddress as any),
+      rememberAddress: false
     }
   })
 
