6ceb41d958e5f343e4b09eb11b56e09984518799
Bryan De La Cruz <bryan.delacruza@gmail.com>
2024-03-31 11:54:37 -0500

    S22-Colocar orden: Total,SubTotal,Impuesto

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -34,7 +34,19 @@ export const placeOrder = async (productIds: ProductToOrder[], address: Address)
   // Calcular los montos
   const itemsInOrder = productIds.reduce((count, p) => count + p.quantity, 0)
 
-  console.log({ itemsInOrder })
-
   // Los totales de tax, subtotal y total
+  const { subtotal, tax, total } = productIds.reduce((totals, item) => {
+    const productQuantity = item.quantity
+    const product = products.find(p => p.id === item.productId)
+
+    if (!product) throw new Error(`${item.productId} no existe - 500`)
+
+    const subTotal = product.price * productQuantity
+    totals.subtotal += subTotal
+    totals.tax += subTotal * 0.15
+    totals.total += subTotal * 1.15
+
+    return totals
+  }, { subtotal: 0, tax: 0, total: 0 })
+  console.log({ subtotal, tax, total })
 }
