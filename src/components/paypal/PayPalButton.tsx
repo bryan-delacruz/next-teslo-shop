959f7bb382d52b351d6f2b631ba3d0cc5732d09b
Bryan De La Cruz <bryan.delacruza@gmail.com>
2024-04-27 20:43:36 -0500

    S23-Mostrar skeleton mientras se carga

--- src/components/paypal/PayPalButton.tsx
+++ src/components/paypal/PayPalButton.tsx
@@ -1,8 +1,19 @@
 'use client'
 
-import { PayPalButtons } from "@paypal/react-paypal-js"
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js"
 
 export const PayPalButton = () => {
+
+  const [{ isPending }] = usePayPalScriptReducer()
+
+  if (isPending) {
+    return (
+      <div className="animate-pulse mb-14">
+        <div className="h-11 bg-gray-300 rounded"/>
+        <div className="h-11 bg-gray-300 rounded mt-2"/>
+      </div>
+    )
+  }
   return (
     <PayPalButtons />
   )
