29ea4f915405653c0ac420f7ec24979c4cb84691
Bryan De La Cruz <bryan.delacruza@gmail.com>
2024-05-08 22:09:34 -0500

    S23-Server Action | Verificar Pago - parte 2

--- src/actions/payments/paypal-check-payment.ts
+++ src/actions/payments/paypal-check-payment.ts
@@ -1,6 +1,51 @@
 'use server'
 
 export const paypalCheckPayment = async (paypalTransactionId: string) => {
-  console.log({paypalTransactionId});
-  
+
+  const authToken = await getPaypalBearerToken()
+
+  console.log(authToken);
+
+  if (!authToken) {
+    return {
+      ok: false,
+      message: "No se pudo obtener token de verificación"
+    }
+  }
+
+}
+
+const getPaypalBearerToken = async (): Promise<string | null> => {
+
+  const PAYPAL_CLIENT_ID = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID
+  const PAYPAL_SECRET = process.env.PAYPAL_SECRET
+  const oauth2Url = process.env.PAYPAL_OAUTH_URL ?? ""
+
+  const base64Token = Buffer.from(
+    `${PAYPAL_CLIENT_ID}:${PAYPAL_SECRET}`,
+    'utf-8'
+  ).toString('base64')
+
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+  myHeaders.append("Authorization", `Basic ${base64Token}`);
+
+  const urlencoded = new URLSearchParams();
+  urlencoded.append("grant_type", "client_credentials");
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: urlencoded
+  };
+
+  try {
+    const result = await fetch(oauth2Url, requestOptions).then(r => r.json())
+    return result.access_token
+
+  } catch (error) {
+    console.log(error);
+    return null
+  }
+
 }
