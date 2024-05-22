83e17f9594e930008b49d20481a1e8bb1a3a1fc1
Bryan De La Cruz <bryan.delacruza@gmail.com>
2024-05-22 00:27:23 -0500

    S24-Cargar a Cloudinary

--- src/actions/product/create-update-product.ts
+++ src/actions/product/create-update-product.ts
@@ -5,6 +5,10 @@ import { Gender, Product, Size } from '@prisma/client';
 import { revalidatePath } from 'next/cache';
 import { z } from 'zod'
 
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config(process.env.CLOUDINARY_URL ?? '')
+
 const productSchema = z.object({
   id: z.string().uuid().optional().nullable(),
   title: z.string().min(3).max(255),
@@ -81,7 +85,10 @@ export const createUpdateProduct = async (formData: FormData) => {
       }
 
       if (formData.getAll('images')) {
-        console.log(formData.getAll('images'))
+        const images = await uploadImages(formData.getAll('images') as File[])
+
+        console.log(images);
+
       }
 
 
@@ -105,5 +112,38 @@ export const createUpdateProduct = async (formData: FormData) => {
       ok: false,
       message: 'Revisar los logs, no se pudo actualizar'
     }
+  }
+}
+
+const uploadImages = async (images: File[]) => {
+
+  try {
+    const uploadPromises = images.map(async (image) => {
+
+      try {
+
+        const buffer = await image.arrayBuffer()
+        const base64Image = Buffer.from(buffer).toString('base64')
+
+        return cloudinary.uploader.upload(`data:image/png;base64,${base64Image}`)
+          .then(r => r.secure_url)
+
+      } catch (error) {
+
+        console.log(error)
+        return null
+
+      }
+    })
+
+    const uploadedImages = await Promise.all(uploadPromises)
+
+    return uploadedImages
+
+  } catch (error) {
+
+    console.log(error)
+    return null
+
   }
 }
